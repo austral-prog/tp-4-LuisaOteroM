23c584ed2aded7835cbbbbbae6df304f7110dbb6
LuisaOteroM <loteromonsegur@mail.austral.edu.ar>
2025-05-28 17:41:26 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,9 +1,6 @@
 def leap_year():
-    year = int(imput("Ingrese un año: "))
-    if(year % 4 == 0):
-        if( (year % 100 ==0) and (year % 400 ==0) )
-            print(f"El año {year} es bisiesto")
-        else:
-            print(f"El año {year} no es bisiesto")
+    year = int(imput("Ingrese un año:"))
+    if year % 400 == 0:
+        print(f"El año {year} es bisiesto")
     else:
         print(f"El año {year} no es bisiesto")
